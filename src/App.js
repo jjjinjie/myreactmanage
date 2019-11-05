f4bed15fa01f488d9adf59c3c730af394880b625
jjjinjie <15952021753@163.com>
2019-11-05 14:10:22 +0800

    test

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="http://www.baidu.com"
+          href="http://www.sogou.com"
           target="_blank"
           rel="noopener noreferrer"
         >
